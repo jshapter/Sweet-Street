c45610d227e0982e5ec03e420a6f1c2a8df21740
jshapter <j.a.shapter@gmail.com>
2023-09-21 21:41:40 +0100

    Colour theme and typography

--- app/src/main/java/com/example/sweetstreet/ui/theme/Type.kt
+++ app/src/main/java/com/example/sweetstreet/ui/theme/Type.kt
@@ -2,9 +2,15 @@ package com.example.sweetstreet.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.sweetstreet.R
+
+val Poiret = FontFamily(
+    Font(R.font.poiret)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,15 +20,16 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = Poiret,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 28.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
-    ),
+    )
+    /* Other default text styles to override
+    ,
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
